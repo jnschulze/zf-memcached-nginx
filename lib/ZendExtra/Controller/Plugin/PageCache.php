70cad39cb648ceacb65f060593ddebcfcf62b2cc
Jan N. Schulze <me@jns.io>
2013-05-16 15:43:52 +0200

    Update PageCache.php

--- lib/ZendExtra/Controller/Plugin/PageCache.php
+++ lib/ZendExtra/Controller/Plugin/PageCache.php
@@ -8,11 +8,32 @@ class ZendExtra_Controller_Plugin_PageCache extends Zend_Controller_Plugin_Abstr
     {
         $this->_cacheFrontend = $frontend;
     }
+    
+    protected function _skip(Zend_Controller_Request_Abstract $request)
+    {
+        if($this->getResponse()->isException())
+        {
+            return true;
+        }
+
+        if (!($this->_isDispatchable($request)))
+        {
+           return true;
+        }
+
+        return false;
+    }
+
+    protected function _isDispatchable(Zend_Controller_Request_Abstract $request)
+    {
+        return Zend_Controller_Front::getInstance()->getDispatcher()->isDispatchable($request);
+    }
 
     public function routeShutdown(Zend_Controller_Request_Abstract $request)
     {
         $enabled = Zend_Registry::get('CACHE_ENABLED');
-        if($enabled && !Zend_Session::sessionExists())
+
+        if($enabled && !Zend_Session::sessionExists() && !$this->_skip($request))
         {
             $this->_cacheFrontend->start();
         }
